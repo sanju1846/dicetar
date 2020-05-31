0cd76515304be5f3638d5fb5b2a2ba10b21670ae
Sanju Paul <sanju1846@gmail.com>
2020-05-31 20:11:16 +0530

    Uploaded initial files of dicetar game

---
+++ index.js
@@ -0,0 +1,32 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //random number 1 to 6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //  random dice number to dice image
+
+var randomImageSource = "images/" + randomDiceImage;  // images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 Wins!!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!!";
+}
+else {
+  document.querySelector("h1").innerHTML = "Match Draw!!";
+}
+
+
+function reloaddicee() {
+location.reload();
+}
